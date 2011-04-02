c9aa17c5e075036c79bc6779f847c6645fa8d69c
Lisa Glendenning <lglenden@cs.washington.edu>
2011-04-01 18:53:31 -0700

    standardized pavement

--- pavement.py
+++ pavement.py
@@ -1,32 +1,55 @@
+# @copyright
+# @license
 
 # Paver project
-# http://www.blueskyonmars.com/projects/paver/index.html
+# http://paver.github.com/
 
 # Paver works a bit like Make or Rake. 
 # To use Paver, you run paver <taskname> and the paver command 
 # will look for a pavement.py file in the current directory
 
-import ConfigParser, os, os.path, sys
+import sys, os
 
-from paver.easy import *
-from paver.setuputils import setup
+import paver.easy
+import paver.setuputils
 
-#
-# configure
-#
-# (for Paver to work probably, this needs to be global)
-conf = ConfigParser.SafeConfigParser()
-with open('pavement.ini') as f:
-    conf.readfp(f)
-options.conf = conf
-opts = dict(options.conf.items('setup'))
-for opt in opts:
-    val = opts[opt]
-    opts[opt] = eval(val)
-setup(**opts)
+NAME = 'pypetri'
 
-@task
-@needs('paver.misctasks.minilib', 
-       'paver.misctasks.generate_setup',)
+def configure():
+    cwd = os.getcwd()
+    sys.path.insert(0, os.path.join(cwd, 'source'))
+    package = __import__('pypetri')
+    del sys.path[0]
+    setup = dict(
+                 version=package.__version__,
+                 url=package.__url__,
+                 author=package.__author__,
+                 author_email=package.__author_email__,
+                 license=package.__license__,
+                 keywords=', '.join([repr(k) for k in package.__keywords__]),
+                 install_requires=package.__requires__,
+                 extras_require=package.__extras__,
+                )
+    return setup
+
+paver.setuputils.setup(
+        name=NAME,
+        packages=paver.setuputils.find_packages('source'),
+        package_dir = {'':'source'},
+        **configure())
+
+@paver.easy.task
+@paver.easy.needs('paver.misctasks.generate_setup',
+       'paver.misctasks.minilib',)
 def sdist():
-    call_task('setuptools.command.sdist')
+    """Overrides sdist to make sure that our setup.py is generated."""
+    
+    # Create distribution manifest
+    includes = ['setup.py', 'paver-minilib.zip']
+    lines = ['include %s' % ' '.join(includes),
+             'recursive-include source *.py',]
+    with open('MANIFEST.in', 'w') as f:
+        f.write('\n'.join(lines))
+        f.write('\n')
+
+    paver.easy.call_task('setuptools.command.sdist')
---
+++ source/pypetri/__init__.py
@@ -0,0 +1,14 @@
+# @copyright
+# @license
+
+# Package metadata
+
+__version__ = "0.1"
+__url__ = ''
+__author__ = 'Lisa Glendenning'
+__author_email__ = 'author@lisa.glendenning.name'
+__license__ = 'MIT/X'
+__keywords__ = ['petri net']
+__requires__ = ['Trellis >=0.7a2']
+__extras__ = {'pypetri.graph': ['networkx>=1.1'], \
+              'pypetri.graph.viz': ['pygraphviz>=0.99.1'],}
