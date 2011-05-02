1437fca618506516ef462550b0a50a1a4123bdef
Lisa Glendenning <lglenden@cs.washington.edu>
2011-05-02 15:16:59 -0700

    refactored example

--- examples/traffic_lights.py
+++ examples/traffic_lights.py
@@ -3,6 +3,7 @@
 
 import types
 
+from pypetri import trellis
 from pypetri.collections import flow
 
 #############################################################################
@@ -16,32 +17,62 @@ class Light(flow.Network):
     
     CONDITIONS = ["RED", "GREEN", "YELLOW",]
     TRANSITIONS = ["RED2GREEN", "GREEN2YELLOW", "YELLOW2RED",]
+    
+    red = trellis.attr(None)
+    green = trellis.attr(None)
+    yellow = trellis.attr(None)
+    red2green = trellis.attr(None)
+    green2yellow = trellis.attr(None)
+    yellow2red = trellis.attr(None)
 
-    def __new__(cls, *args, **kwargs):
-        self = super(Light, cls).__new__(cls, *args, **kwargs)
-        self.conditions = []
+    def __init__(self, *args, **kwargs):
         for name in self.CONDITIONS:
-            capacity = 1 if name == self.CONDITIONS[0] else 2
-            condition = self.Condition(minimum=0, maximum=capacity)
-            self.conditions.append(condition)
-        self.transitions = []
+            if name.lower() not in kwargs:
+                capacity = 1 if name == self.CONDITIONS[0] else 2
+                marking = 1 if name == self.CONDITIONS[0] else 0
+                condition = self.Condition(minimum=0, maximum=capacity, marking=marking)
+                kwargs[name.lower()] = condition
         for name in self.TRANSITIONS:
-            transition = self.Transition()
-            transition.predicate = types.MethodType(lambda self, flow: flow == self.maximum, transition, transition.__class__)
-            self.transitions.append(transition)
-        for vertices in self.conditions, self.transitions:
-            self.vertices.update(vertices)
-        
+            if name.lower() not in kwargs:
+                transition = self.Transition()
+                transition.predicate = types.MethodType(lambda self, flow: flow == self.maximum, transition, transition.__class__)
+                kwargs[name.lower()] = transition
+        super(Light, self).__init__(*args, **kwargs)
+    
+    @trellis.maintain
+    def links(self):
+        conditions = [getattr(self, name.lower()) for name in self.CONDITIONS]
+        transitions = [getattr(self, name.lower()) for name in self.TRANSITIONS]
+        for vertices in (conditions, transitions,):
+            for v in vertices:
+                if v not in self.vertices:
+                    self.vertices.add(v)
+                    
         # arcs create a circular network
-        arcs = []
-        for i in xrange(len(self.conditions)):
-            j = i+1 if i < len(self.conditions)-1 else 0
-            for arc in self.chain((self.conditions[i], self.transitions[i], self.conditions[j],)):
-                arcs.append(arc)
-        
-        # and, initial marking
-        self.conditions[0].marking = 1
-        return self
+        for i in xrange(len(conditions)):
+            j = i+1 if i < len(conditions)-1 else 0
+            chain = (conditions[i], transitions[i], conditions[j],)
+            for i in chain[1].inputs:
+                if i.input is chain[0]:
+                    break
+            else:
+                for i in chain[1].inputs:
+                    if i.input is None:
+                        chain[0].outputs.add(i)
+                        break
+                else:
+                    self.link(chain[0], chain[1])
+            for o in chain[1].outputs:
+                if o.output is chain[2]:
+                    break
+            else:
+                for o in chain[1].outputs:
+                    if o.output is None:
+                        chain[2].inputs.add(o)
+                        break
+                else:
+                    self.link(chain[1], chain[2])
+
 
 #############################################################################
 #############################################################################
@@ -50,48 +81,64 @@ class Light(flow.Network):
 # Net composed of two light subnets
 #
 
-class FourWayIntersection(flow.Network):
+class Intersection(flow.Network):
 
     # Global start state
     CONDITIONS = ['START',]
 
-    # Light names
-    LIGHTS = ['A', 'B',]
+    start = trellis.attr(None)
+    lights = trellis.make(trellis.Set)
 
-    def __new__(cls, *args, **kwargs):
-        self = super(FourWayIntersection, cls).__new__(cls, *args, **kwargs)
-        
-        self.conditions = []
+    def __init__(self, ways=2, *args, **kwargs):
         for name in self.CONDITIONS:
-            capacity = 1
-            condition = self.Condition(minimum=0, maximum=capacity)
-            self.conditions.append(condition)
-            
-        self.lights = []
-        for name in self.LIGHTS:
-            light = Light()
-            self.lights.append(light)
-        for vertices in self.conditions, self.lights:
-            self.vertices.update(vertices)
+            if name.lower() not in kwargs:
+                capacity = 1
+                marking = 1
+                condition = self.Condition(minimum=0, maximum=capacity, marking=marking)
+                kwargs[name.lower()] = condition
+        if 'lights' not in kwargs:
+            kwargs['lights'] = trellis.Set([Light() for i in xrange(ways)])
+        super(Intersection, self).__init__(*args, **kwargs)
         
-        start = self.conditions[0]
-        arcs = []
+
+    @trellis.maintain
+    def links(self):
+        conditions = self.start, 
+        for vertices in (conditions, self.lights,):
+            for v in vertices:
+                if v not in self.vertices:
+                    self.vertices.add(v)
+
+        start = self.start
         for light in self.lights:
             # input and output to light
-            pairs = ((start, light.transitions[0]),
-                     (light.transitions[-1], start,),)
-            for pair in pairs:
-                arcs.append(self.link(*pair))
-
-        # and, initial marking
-        self.conditions[0].marking = 1
-
-        return self
-
+            output = getattr(light, light.TRANSITIONS[0].lower())
+            input = getattr(light, light.TRANSITIONS[-1].lower())
+            for i in start.inputs:
+                if i.input is input:
+                    break
+            else:
+                for i in start.inputs:
+                    if i.input is None:
+                        inputs.outputs.add(i)
+                        break
+                else:
+                    self.link(input, start)
+            for o in start.outputs:
+                if o.output is output:
+                    break
+            else:
+                for o in start.outputs:
+                    if o.output is None:
+                        output.inputs.add(o)
+                        break
+                else:
+                    self.link(start, output)
+        
 #############################################################################
 #############################################################################
 
-Network = FourWayIntersection
+Network = Intersection
 
 #############################################################################
 #############################################################################
--- tests/examples/test_traffic_lights.py
+++ tests/examples/test_traffic_lights.py
@@ -12,14 +12,16 @@ class TestCaseTrafficLights(unittest.TestCase):
     
     def initialized(self, network):
         for light in network.lights:
-            events = [e for e in light.next(light.transitions)]
+            transitions = [getattr(light, name.lower()) for name in light.TRANSITIONS]
+            events = [e for e in light.next(transitions)]
             self.assertEqual(len(events), 1)
     
     def test_example(self):
         network = traffic_lights.Network()
         self.initialized(network)
         for light in network.lights:
-            for t in light.transitions:
+            transitions = [getattr(light, name.lower()) for name in light.TRANSITIONS]
+            for t in transitions:
                 events = [e for e in t.next()]
                 self.assertEqual(len(events), 1)
                 output = events[0]()
