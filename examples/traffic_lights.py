384f1c05c449deb5ff3a33b1187dc1b9c4c8ea4c
Lisa Glendenning <lglenden@cs.washington.edu>
2011-06-10 15:14:19 -0700

    refs #174

--- examples/traffic_lights.py
+++ examples/traffic_lights.py
@@ -6,13 +6,13 @@ import functools
 
 from pypetri import trellis
 from pypetri import net
-from pypetri.collections import flow
+from pypetri.collections import flow, sets
 
 #############################################################################
 #############################################################################
 
 #
-# Flow must equal maximum
+# Flow Network such that the flow must equal the maximum
 #
 
 class ExactFlow(flow.Network):
@@ -31,6 +31,9 @@ class ExactFlow(flow.Network):
             kwargs['demux'] = flow.Transition.Demultiplexer(predicate=self.ismax)
         return super(ExactFlow, self).Transition(*args, **kwargs)
 
+#############################################################################
+#############################################################################
+
 #
 # Net for a single light
 #
@@ -39,45 +42,46 @@ class Light(ExactFlow):
     
     CONDITIONS = ["RED", "GREEN", "YELLOW",]
     TRANSITIONS = ["RED2GREEN", "GREEN2YELLOW", "YELLOW2RED",]
+    
+    def Condition(self, name, **kwargs):
+        capacity = 1 if name == self.CONDITIONS[0] else 2
+        marking = 1 if name == self.CONDITIONS[0] else 0
+        return super(Light, self).Condition(maximum=capacity, marking=marking, **kwargs)
 
-    red = trellis.make(None)
-    green = trellis.make(None)
-    yellow = trellis.make(None)
-    red2green = trellis.make(None)
-    green2yellow = trellis.make(None)
-    yellow2red = trellis.make(None)
+    def Output(self, name, **kwargs):
+        CONDITIONS = self.CONDITIONS
+        TRANSITIONS = self.TRANSITIONS
+        if name in CONDITIONS:
+            i = CONDITIONS.index(name)
+            j = i
+            next = TRANSITIONS[j]
+        else:
+            i = TRANSITIONS.index(name)
+            j = 0 if (i == len(TRANSITIONS)-1) else i+1
+            next = CONDITIONS[j]
+        source = getattr(self, name.lower())
+        sink = getattr(self, next.lower())
+        for o in source.outputs:
+            if o.output is sink:
+                break
+        else:
+            self.Arc(source, sink, **kwargs)
+        return source
 
-    def __init__(self, *args, **kwargs):
-        names = self.CONDITIONS
-        for v in names:
-            attr = v.lower()
-            if attr not in kwargs:
-                capacity = 1 if v == names[0] else 2
-                marking = 1 if v == names[0] else 0
-                condition = self.Condition(maximum=capacity, marking=marking,)
-                kwargs[attr] = condition
-        names = self.TRANSITIONS
-        for v in names:
-            attr = v.lower()
-            if attr not in kwargs:
-                transition = self.Transition()
-                kwargs[attr] = transition
-        super(Light, self).__init__(*args, **kwargs)
-        conditions = []
-        transitions = []
-        for ls, names in ((conditions, self.CONDITIONS), (transitions, self.TRANSITIONS,),):
-            for name in names:
-                v = getattr(self, name.lower())
-                ls.append(v)
+    red = trellis.maintain(rule=lambda self: self.Output('RED'), 
+                           make=lambda self: self.Condition('RED'))
+    green = trellis.maintain(rule=lambda self: self.Output('GREEN'), 
+                             make=lambda self: self.Condition('GREEN'))
+    yellow = trellis.maintain(rule=lambda self: self.Output('YELLOW'), 
+                              make=lambda self: self.Condition('YELLOW'))
 
-        # arcs create a circular network
-        for i in xrange(len(conditions)):
-            j = i+1 if i < len(conditions)-1 else 0
-            for pair in ((conditions[i], transitions[i],),
-                         (transitions[i], conditions[j],),):
-                arc = self.Arc(*pair)
-
-
+    red2green = trellis.maintain(rule=lambda self: self.Output('RED2GREEN'), 
+                                 make=lambda self: self.Transition())
+    green2yellow = trellis.maintain(rule=lambda self: self.Output('GREEN2YELLOW'), 
+                                    make=lambda self: self.Transition())
+    yellow2red = trellis.maintain(rule=lambda self: self.Output('YELLOW2RED'), 
+                                  make=lambda self: self.Transition())
+    
 #############################################################################
 #############################################################################
 
@@ -90,32 +94,31 @@ class Intersection(ExactFlow):
     # Global start state
     CONDITIONS = ['START',]
 
-    start = trellis.make(None)
-    lights = trellis.make(trellis.Set)
-
-    def __init__(self, ways=2, *args, **kwargs):
-        for name in self.CONDITIONS:
-            attr = name.lower()
-            if attr not in kwargs:
-                capacity = 1
-                marking = 1
-                condition = self.Condition(maximum=capacity, marking=marking)
-                kwargs[attr] = condition
-        attr = 'lights'
-        if attr not in kwargs:
-            kwargs[attr] = trellis.Set([Light() for i in xrange(ways)])
-        super(Intersection, self).__init__(*args, **kwargs)
-
-        #
-        # arcs
-        #
-
-        start = self.start
+    lights = trellis.make(sets.Set)
+    
+    @trellis.maintain(make=lambda self: self.Condition(maximum=1, marking=1))
+    def start(self):
+        condition = self.start
         for light in self.lights:
             input = getattr(light, light.TRANSITIONS[-1].lower())
             output = getattr(light, light.TRANSITIONS[0].lower())
-            for pair in ((input, start), (start, output)):
-                arc = self.Arc(*pair)
+            for o in condition.outputs:
+                if o.output is output:
+                    break
+            else:
+                self.Arc(condition, output)
+            for i in condition.inputs:
+                if i.input is input:
+                    break
+            else:
+                self.Arc(input, condition)
+        return condition
+
+    def __init__(self, ways=2, *args, **kwargs):
+        attr = 'lights'
+        if attr not in kwargs:
+            kwargs[attr] = sets.Set([Light() for i in xrange(ways)])
+        super(Intersection, self).__init__(*args, **kwargs)
 
 #############################################################################
 #############################################################################
