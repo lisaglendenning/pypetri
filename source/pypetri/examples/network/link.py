edaf6088f5d96c298c9fbf4502baff35ddc7c0f4
Lisa Glendenning <lisa@glendenning.net>
2012-08-01 14:04:52 -0700

    updates

--- source/pypetri/examples/network/link.py
+++ source/pypetri/examples/network/link.py
@@ -12,48 +12,7 @@ Messages in the input queue can be delayed, duplicated, or dropped.
 from __future__ import absolute_import
 
 from pypetri import net, trellis, operators
-from pypetri.collections import collection, queue
-
-#############################################################################
-#############################################################################
-
-class Queue(collection.Collection):
-
-    marking = trellis.make(queue.Queue)
-    
-    @trellis.compute
-    def __getitem__(self):
-        return self.marking.__getitem__
-    
-    @trellis.compute
-    def enqueue(self):
-        return self.marking.enqueue
-    
-    @trellis.compute
-    def dequeue(self):
-        return self.marking.dequeue
-    
-    @trellis.compute
-    def head(self):
-        return self.marking.head
-    
-    @trellis.compute
-    def tail(self):
-        return self.marking.tail
-    
-    @trellis.modifier
-    def pull(self, item):
-        self.dequeue()
-        return item
-    
-    @trellis.modifier
-    def send(self, *args):
-        for arg in args:
-            self.enqueue(arg)
-    
-    def next(self):
-        if len(self):
-            yield self.Event(self.pull, self.head)
+from .queue import *
 
 #############################################################################
 #############################################################################
